cc86e236ecf87d4462ea3ca6d22b9d86ad65a04e
Linky141 <tomasz_bielas@yahoo.com>
2020-12-13 23:19:22 +0000

    external thread to I2C

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,42 +1,70 @@
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+import com.pi4j.io.i2c.impl.I2CDeviceImpl;
+import com.pi4j.wiringpi.I2C;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MainWindow  extends JFrame implements ActionListener {
 
     //region VARIABLES
 
-
     //region Window Controlls
-    //region Window
-
+    //region Buttons
+        JButton btnReload;
+    //endregion
+    //region Lables
+        public JLabel configPortLbl;
+    //endregion
+    //region Textbox
+        JTextArea tbxTmp;
+    //endregion
+    //endregion
 
     //endregion
 
+    I2CThread i2cThread = new I2CThread(this);
 
-    //endregion
-    //endregion
 
-public MainWindow(){
+public MainWindow() throws IOException, I2CFactory.UnsupportedBusNumberException {
     //region Window
     setSize(640,480);
     setTitle("NewWindow");
     setLayout(null);
     //endregion
 
+    //region Buttons
+    btnReload = new JButton("REFRESH");
+    btnReload.setBounds(10,50,50,30);
+    btnReload.addActionListener(this);
+    add(btnReload);
+    //endregion
+
+    //region Lables
+    configPortLbl = new JLabel("---");
+    configPortLbl.setBounds(10,200,200,30);
+    add(configPortLbl);
+    //endregion
+
+    //region Textbox
+    tbxTmp = new JTextArea();
+    tbxTmp.setBounds(10,10,500,30);
+    add(tbxTmp);
+    //endregion
 
 }
 
 
 
-
-
-
-
-
-
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        if(actionEvent.getSource()==btnReload) {
+          i2cThread.start();
+        }
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,11 @@
+import com.pi4j.io.i2c.I2CFactory;
+
 import javax.swing.*;
+import java.io.IOException;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException {
         MainWindow mainWindow = new MainWindow();
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.setVisible(true);
